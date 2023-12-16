adcc22d3da5961e71e46c64246385cecc2bc2f8b
The-Epic <epicebic@outlook.com>
2023-12-16 14:49:14 +0000

    more pdc

---
+++ pineapple-core/src/main/java/sh/miles/pineapple/pdc/PineappleDataType.java
@@ -0,0 +1,7 @@
+package sh.miles.pineapple.pdc;
+
+import com.jeff_media.morepersistentdatatypes.DataType;
+import com.jeff_media.morepersistentdatatypes.DataType_1_18_1;
+
+public interface PineappleDataType extends DataType, DataType_1_18_1 {
+}
