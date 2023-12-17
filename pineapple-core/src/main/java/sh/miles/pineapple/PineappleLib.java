62bfc6960c25935978eff7562486f5bd457a02bb
The-Epic <epicebic@outlook.com>
2023-12-17 16:02:23 +0000

    Main api class

--- pineapple-core/src/main/java/sh/miles/pineapple/PineappleLib.java
+++ pineapple-core/src/main/java/sh/miles/pineapple/PineappleLib.java
@@ -1,16 +1,47 @@
 package sh.miles.pineapple;
 
+import org.bukkit.plugin.Plugin;
+import sh.miles.pineapple.config.ConfigurationManager;
+
+import java.util.logging.Logger;
+
 public class PineappleLib {
-    private static final PineappleLib INSTANCE = new PineappleLib();
+    private static  PineappleLib instance;
 
-    // TODO add spigot plugin here
+    private final Plugin plugin;
+    private final ConfigurationManager configManager;
 
-    private PineappleLib() {
+    public PineappleLib(Plugin plugin) {
+        this.plugin = plugin;
+        this.configManager = new ConfigurationManager(plugin);
     }
 
+    public static void initialize(Plugin plugin) {
+        instance = new PineappleLib(plugin);
+        // Add inv events
+    }
 
+    public static void cleanup() {
+        instance = null;
+    }
 
     public static PineappleLib getInstance() {
-        return INSTANCE;
+        if (instance == null) {
+            throw new IllegalStateException("Not initialized");
+        }
+
+        return instance;
+    }
+
+    public static Plugin getPluginInstance() {
+        return getInstance().plugin;
+    }
+
+    public static Logger getLogger() {
+        return getInstance().plugin.getLogger();
+    }
+
+    public static ConfigurationManager getConfigurationManager() {
+        return getInstance().configManager;
     }
 }
