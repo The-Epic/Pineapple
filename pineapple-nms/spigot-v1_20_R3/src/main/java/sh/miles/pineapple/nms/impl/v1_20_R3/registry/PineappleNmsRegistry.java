3def17a046a3b4bc0f67fef9551c9fb3838e309e
Miles Holder <mwholder2005@gmail.com>
2023-12-19 13:31:58 -0600

    registry hotfix

--- pineapple-nms/spigot-v1_20_R3/src/main/java/sh/miles/pineapple/nms/impl/v1_20_R3/registry/PineappleNmsRegistry.java
+++ pineapple-nms/spigot-v1_20_R3/src/main/java/sh/miles/pineapple/nms/impl/v1_20_R3/registry/PineappleNmsRegistry.java
@@ -13,8 +13,10 @@ import sh.miles.pineapple.collection.registry.FrozenRegistry;
 import sh.miles.pineapple.collection.registry.RegistryKey;
 import sh.miles.pineapple.nms.api.menu.MenuType;
 import sh.miles.pineapple.nms.api.world.damagesource.DamageEffect;
+import sh.miles.pineapple.nms.api.world.damagesource.DamageType;
 import sh.miles.pineapple.nms.impl.v1_20_R3.inventory.PineappleMenuType;
 import sh.miles.pineapple.nms.impl.v1_20_R3.world.damagesource.PineappleDamageEffect;
+import sh.miles.pineapple.nms.impl.v1_20_R3.world.damagesource.PineappleDamageType;
 
 import java.util.Arrays;
 import java.util.function.BiFunction;
@@ -22,13 +24,8 @@ import java.util.stream.Collectors;
 
 public final class PineappleNmsRegistry<P extends RegistryKey<NamespacedKey>, M> extends FrozenRegistry<P, NamespacedKey> {
 
-    public PineappleNmsRegistry(Class<? super P> interfaceClass, Registry<M> minecraftRegistry, BiFunction<NamespacedKey, M, P> minecraftToBukkit) {
+    public PineappleNmsRegistry(Registry<M> minecraftRegistry, BiFunction<NamespacedKey, M, P> minecraftToBukkit) {
         super(() -> {
-                    try {
-                        Class.forName(interfaceClass.getName());
-                    } catch (ClassNotFoundException e) {
-                        throw new IllegalStateException("Could not load registry class " + interfaceClass, e);
-                    }
                     return minecraftRegistry.keySet()
                             .stream()
                             .collect(Collectors.toMap(
@@ -39,13 +36,8 @@ public final class PineappleNmsRegistry<P extends RegistryKey<NamespacedKey>, M>
         );
     }
 
-    public PineappleNmsRegistry(Class<? super P> interfaceClass, M[] minecraftEnumEntries, BiFunction<NamespacedKey, M, P> minecraftToBukkit) {
+    public PineappleNmsRegistry(M[] minecraftEnumEntries, BiFunction<NamespacedKey, M, P> minecraftToBukkit) {
         super(() -> {
-            try {
-                Class.forName(interfaceClass.getName());
-            } catch (ClassNotFoundException e) {
-                throw new IllegalStateException("Could not load registry class " + interfaceClass, e);
-            }
             return Arrays.stream(minecraftEnumEntries).collect(Collectors.toMap(
                     (M value) -> NamespacedKey.minecraft(value.toString().toLowerCase()),
                     (M value) -> minecraftToBukkit.apply(NamespacedKey.minecraft(value.toString().toLowerCase()), value)
@@ -56,10 +48,13 @@ public final class PineappleNmsRegistry<P extends RegistryKey<NamespacedKey>, M>
     public static <P extends RegistryKey<NamespacedKey>> FrozenRegistry<?, ?> makeRegistry(Class<? super P> interfaceClass) {
         final RegistryAccess access = ((CraftServer) Bukkit.getServer()).getHandle().getServer().registryAccess();
         if (interfaceClass == MenuType.class) {
-            return new PineappleNmsRegistry<>(MenuType.class, access.registryOrThrow(Registries.MENU), PineappleMenuType::new);
+            return new PineappleNmsRegistry<>(access.registryOrThrow(Registries.MENU), PineappleMenuType::new);
         }
         if (interfaceClass == DamageEffect.class) {
-            return new PineappleNmsRegistry<>(DamageEffect.class, DamageEffects.values(), PineappleDamageEffect::new);
+            return new PineappleNmsRegistry<>(DamageEffects.values(), PineappleDamageEffect::new);
+        }
+        if (interfaceClass == DamageType.class) {
+            return new PineappleNmsRegistry<>(access.registryOrThrow(Registries.DAMAGE_TYPE), PineappleDamageType::new);
         }
 
         throw new IllegalStateException("No such registry %s found".formatted(interfaceClass.getName()));
