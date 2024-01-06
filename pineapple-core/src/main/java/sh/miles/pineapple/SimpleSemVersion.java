3eb07184041d3eb3a336e4abfa1b7bcced252d11
The-Epic <epicebic@outlook.com>
2024-01-06 18:36:39 +0000

    impl

--- pineapple-core/src/main/java/sh/miles/pineapple/SimpleSemVersion.java
+++ pineapple-core/src/main/java/sh/miles/pineapple/SimpleSemVersion.java
@@ -6,10 +6,25 @@ public class SimpleSemVersion {
     private int minor;
     private int patch;
 
-    public SimpleSemVersion(int major, int minor, int patch) {
+    private boolean alpha;
+    private boolean beta;
+    private boolean snapshot;
+    private boolean hotfix;
+    private boolean release;
+
+    public SimpleSemVersion(int major, int minor, int patch, boolean alpha, boolean beta, boolean snapshot, boolean hotfix, boolean release) {
         this.major = major;
         this.minor = minor;
         this.patch = patch;
+        this.alpha = alpha;
+        this.beta = beta;
+        this.snapshot = snapshot;
+        this.hotfix = hotfix;
+        this.release = release;
+    }
+
+    public SimpleSemVersion(int major, int minor, int patch) {
+        this(major, minor, patch, false, false, false, false, false);
     }
 
     /**
@@ -19,21 +34,37 @@ public class SimpleSemVersion {
      * @return the parsed SimpleSemVersion object
      */
     public static SimpleSemVersion fromString(String version) {
-        version = version.replaceAll("[^\\d.]", "");
-        String[] parts = version.split("\\.");
+        String[] split = version.split("-");
+        String[] numParts = split[0].split("\\.");
+        String suffix = split.length > 1 ? split[1] : "";
 
-        if (parts.length != 3) {
+        if (numParts.length != 3) {
             throw new IllegalArgumentException("String must be in the format major.minor.patch");
         }
 
-        SimpleSemVersion semVersion = null;
+        SimpleSemVersion semVer;
+
         try {
-            semVersion = new SimpleSemVersion(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]),
-                    Integer.parseInt(parts[2]));
+            semVer = new SimpleSemVersion(Integer.parseInt(numParts[0]), Integer.parseInt(numParts[1]),
+                    Integer.parseInt(numParts[2]));
         } catch (NumberFormatException ex) {
-            throw new IllegalArgumentException("String must be in the format major.minor.patch");
+            throw new IllegalArgumentException("String must be in the format major.minor.patch-SUFFIX (optional suffix)");
         }
-        return semVersion;
+
+        if (!suffix.isEmpty()) {
+            suffix.toLowerCase();
+            switch (suffix) {
+                case "alpha" -> semVer.alpha = true;
+                case "beta" -> semVer.beta = true;
+                case "snapshot" -> semVer.snapshot = true;
+                case "hotfix" -> semVer.hotfix = true;
+                case "release" -> semVer.release = true;
+                default -> throw new IllegalArgumentException(
+                        "Invalid suffix, must be alpha, beta, snapshot, hotfix, or release");
+            }
+        }
+
+        return semVer;
     }
 
     /**
@@ -50,12 +81,40 @@ public class SimpleSemVersion {
         } else if (this.patch != other.patch) {
             return this.patch > other.patch;
         }
+
+        // ALPHA -> BETA -> SNAPSHOT -> HOTFIX -> RELEASE
+        if (this.release && !other.release) {
+            return true;
+        } else if (this.hotfix && !other.release && !other.hotfix) {
+            return true;
+        } else if (this.snapshot && !other.release && !other.hotfix && !other.snapshot) {
+            return true;
+        } else if (this.beta && !other.release && !other.hotfix && !other.snapshot && !other.beta) {
+            return true;
+        } else if (this.alpha && !other.release && !other.hotfix && !other.snapshot && !other.beta && !other.alpha) {
+            return true;
+        }
+
         return false;
     }
 
     @Override
     public String toString() {
-        return this.major + "." + this.minor + "." + this.patch;
+        return this.major + "." + this.minor + "." + this.patch + getSuffix();
     }
 
+    private String getSuffix() {
+        if (alpha) {
+            return "-ALPHA";
+        } else if (beta) {
+            return "-BETA";
+        } else if (snapshot) {
+            return "-SNAPSHOT";
+        } else if (hotfix) {
+            return "-HOTFIX";
+        } else if (release) {
+            return "-RELEASE";
+        }
+        return "";
+    }
 }
---
+++ pineapple-core/src/test/java/sh/miles/pineapple/updater/SimpleSemVersionTest.java
@@ -0,0 +1,93 @@
+package sh.miles.pineapple.updater;
+
+import org.junit.jupiter.api.Test;
+import sh.miles.pineapple.SimpleSemVersion;
+import sh.miles.pineapple.ReflectionUtils;
+
+import java.lang.invoke.MethodHandle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleSemVersionTest {
+
+    private static final MethodHandle MAJOR = ReflectionUtils.getFieldAsGetter(SimpleSemVersion.class, "major");
+    private static final MethodHandle MINOR = ReflectionUtils.getFieldAsGetter(SimpleSemVersion.class, "minor");
+    private static final MethodHandle PATCH = ReflectionUtils.getFieldAsGetter(SimpleSemVersion.class, "patch");
+
+    private static final MethodHandle ALPHA = ReflectionUtils.getFieldAsGetter(SimpleSemVersion.class, "alpha");
+    private static final MethodHandle BETA = ReflectionUtils.getFieldAsGetter(SimpleSemVersion.class, "beta");
+    private static final MethodHandle SNAPSHOT = ReflectionUtils.getFieldAsGetter(SimpleSemVersion.class, "snapshot");
+    private static final MethodHandle HOTFIX = ReflectionUtils.getFieldAsGetter(SimpleSemVersion.class, "hotfix");
+    private static final MethodHandle RELEASE = ReflectionUtils.getFieldAsGetter(SimpleSemVersion.class, "release");
+
+
+    @Test
+    public void test_No_Suffix() {
+        SimpleSemVersion version = SimpleSemVersion.fromString("1.2.3");
+        assertEquals(1, ReflectionUtils.safeInvoke(MAJOR.bindTo(version)));
+        assertEquals(2, ReflectionUtils.safeInvoke(MINOR.bindTo(version)));
+        assertEquals(3, ReflectionUtils.safeInvoke(PATCH.bindTo(version)));
+    }
+
+    @Test
+    public void test_Alpha() {
+        SimpleSemVersion version = SimpleSemVersion.fromString("1.2.3-alpha");
+        assertTrue(((Boolean) ReflectionUtils.safeInvoke(ALPHA.bindTo(version))));
+    }
+
+    @Test
+    public void test_Beta() {
+        SimpleSemVersion version = SimpleSemVersion.fromString("1.2.3-beta");
+        assertTrue(((Boolean) ReflectionUtils.safeInvoke(BETA.bindTo(version))));
+    }
+
+    @Test
+    public void test_Snapshot() {
+        SimpleSemVersion version = SimpleSemVersion.fromString("1.2.3-snapshot");
+        assertTrue(((Boolean) ReflectionUtils.safeInvoke(SNAPSHOT.bindTo(version))));
+    }
+
+    @Test
+    public void test_Hotfix() {
+        SimpleSemVersion version = SimpleSemVersion.fromString("1.2.3-hotfix");
+        assertTrue(((Boolean) ReflectionUtils.safeInvoke(HOTFIX.bindTo(version))));
+    }
+
+    @Test
+    public void test_Release() {
+        SimpleSemVersion version = SimpleSemVersion.fromString("1.2.3-release");
+        assertTrue(((Boolean) ReflectionUtils.safeInvoke(RELEASE.bindTo(version))));
+    }
+
+    @Test
+    public void test_Newer_Than_AlphaBeta() {
+        SimpleSemVersion version = SimpleSemVersion.fromString("1.2.3-beta");
+        SimpleSemVersion other = SimpleSemVersion.fromString("1.2.3-alpha");
+
+        assertTrue(version.isNewerThan(other));
+    }
+
+    @Test
+    public void test_Newer_Than_AlphaSnapshot() {
+        SimpleSemVersion version = SimpleSemVersion.fromString("1.2.3-snapshot");
+        SimpleSemVersion other = SimpleSemVersion.fromString("1.2.3-alpha");
+
+        assertTrue(version.isNewerThan(other));
+    }
+
+    @Test
+    public void test_Newer_Than_AlphaRelease() {
+        SimpleSemVersion version = SimpleSemVersion.fromString("1.2.3-release");
+        SimpleSemVersion other = SimpleSemVersion.fromString("1.2.3-alpha");
+
+        assertTrue(version.isNewerThan(other));
+    }
+
+    @Test
+    public void test_Newer_Than_BetaSnapshot() {
+        SimpleSemVersion version = SimpleSemVersion.fromString("1.2.3-snapshot");
+        SimpleSemVersion other = SimpleSemVersion.fromString("1.2.3-beta");
+
+        assertTrue(version.isNewerThan(other));
+    }
+}
