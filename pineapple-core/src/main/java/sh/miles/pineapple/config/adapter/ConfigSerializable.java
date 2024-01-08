4062d3b218da1714a1ebe66296f64ed36973c854
Epic <EpicEbic@outlook.com>
2024-01-08 17:10:56 +0000

    javadoc

--- pineapple-core/src/main/java/sh/miles/pineapple/config/adapter/ConfigSerializable.java
+++ pineapple-core/src/main/java/sh/miles/pineapple/config/adapter/ConfigSerializable.java
@@ -1,5 +1,10 @@
 package sh.miles.pineapple.config.adapter;
 
+/**
+ * A static deserialize(type) method is needed for this to function
+ *
+ * @param <S> the saved type
+ */
 public interface ConfigSerializable<S> {
     S serialize(S existing, boolean replace);
 }
