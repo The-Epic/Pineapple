bb89706640e386dff8f873a6db1e15abcc19a463
Epic <EpicEbic@outlook.com>
2024-01-09 00:14:52 +0000

    javadocs, remove un-used logger method, delete config mappable as its no longer used

--- pineapple-core/src/main/java/sh/miles/pineapple/config/ConfigurationManager.java
+++ pineapple-core/src/main/java/sh/miles/pineapple/config/ConfigurationManager.java
@@ -1,14 +1,29 @@
 package sh.miles.pineapple.config;
 
 import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.ApiStatus;
 import sh.miles.pineapple.chat.PineappleComponent;
 import sh.miles.pineapple.collection.WeightedRandom;
-import sh.miles.pineapple.config.adapter.*;
+import sh.miles.pineapple.config.adapter.CollectionAdapter;
+import sh.miles.pineapple.config.adapter.ColorAdapter;
+import sh.miles.pineapple.config.adapter.ConfigSerializable;
+import sh.miles.pineapple.config.adapter.ConfigSerializableAdapter;
+import sh.miles.pineapple.config.adapter.EnumAdapter;
+import sh.miles.pineapple.config.adapter.ItemStackAdapter;
+import sh.miles.pineapple.config.adapter.MapAdapter;
+import sh.miles.pineapple.config.adapter.MaterialAdapter;
+import sh.miles.pineapple.config.adapter.NamespacedKeyAdapter;
+import sh.miles.pineapple.config.adapter.NativeAdapter;
+import sh.miles.pineapple.config.adapter.PineappleComponentAdapter;
+import sh.miles.pineapple.config.adapter.PrimitiveAdapter;
+import sh.miles.pineapple.config.adapter.StringAdapter;
+import sh.miles.pineapple.config.adapter.TypeAdapter;
+import sh.miles.pineapple.config.adapter.TypeAdapterString;
+import sh.miles.pineapple.config.adapter.WeightedRandomAdapter;
 
 import java.io.File;
 import java.util.Collection;
@@ -16,11 +31,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
+/**
+ * The ConfigurationManager handles all config creation, loading and TypeAdapters
+ *
+ * @since 1.0.0-SNAPSHOT
+ */
 public class ConfigurationManager {
 
     private final Map<ConfigType<?>, TypeAdapter<?, ?>> adapters = new HashMap<>();
     private final Logger logger;
 
+    @ApiStatus.Internal
     public ConfigurationManager(Plugin plugin) {
         this.logger = plugin.getLogger();
 
@@ -51,22 +72,72 @@ public class ConfigurationManager {
 
     }
 
+    /**
+     * Register a TypeAdapter for data (de)serialization
+     *
+     * @param clazz runtime class
+     * @param adapter TypeAdapter that handles serialization/deserialization
+     * @param <S> serialized type
+     * @param <R> runtime class
+     *
+     * @since 1.0.0-SNAPSHOT
+     */
     public <S, R> void registerTypeAdapter(Class<R> clazz, TypeAdapter<S, R> adapter) {
         adapters.put(new ConfigType<>(clazz), adapter);
     }
 
+    /**
+     * Create an instance based config
+     *
+     * @param file the file to save/load
+     * @param target instance of config class
+     * @return ConfigReloadable<T> instance
+     * @param <T> config class
+     *
+     * @since 1.0.0-SNAPSHOT
+     */
     public <T> ReloadableObject<T> createReloadable(File file, T target) {
         return new ReloadableObject<>(file, target);
     }
 
+    /**
+     *
+     *
+     * @param file the file to save/load
+     * @param target config class
+     * @return ConfigReloadable<T> instance
+     * @param <T> config class
+     *
+     * @since 1.0.0-SNAPSHOT
+     */
     public <T> ReloadableClass<T> createStaticReloadable(File file, Class<T> target) {
         return new ReloadableClass<>(file, target);
     }
 
+    /**
+     * Gets a TypeAdapterString for the given class
+     *
+     * @param clazz runtime class
+     * @return TypeAdapterString instance
+     * @param <S> serialized type
+     * @param <R> runtime class
+     *
+     * @since 1.0.0-SNAPSHOT
+     */
     public <S, R> TypeAdapterString<S, R> getStringAdapter(Class<R> clazz) {
         return getStringAdapter(new ConfigType<>(clazz));
     }
 
+    /**
+     * Gets a TypeAdapterString for the given type
+     *
+     * @param type config type
+     * @return TypeAdapterString instance
+     * @param <S> serialized type
+     * @param <R> runtime class
+     *
+     * @since 1.0.0-SNAPSHOT
+     */
     @SuppressWarnings("unchecked")
     public <S, R> TypeAdapterString<S, R> getStringAdapter(ConfigType<R> type) {
         TypeAdapter<?, R> adapter = getAdapter(type);
@@ -78,10 +149,30 @@ public class ConfigurationManager {
         return null;
     }
 
+    /**
+     * Gets the type adapter for the given class
+     *
+     * @param clazz runtime class
+     * @return TypeAdapter instance
+     * @param <S> serialized type
+     * @param <R> runtime class
+     *
+     * @since 1.0.0-SNAPSHOT
+     */
     public <S, R> TypeAdapter<S, R> getAdapter(Class<R> clazz) {
         return getAdapter(new ConfigType<>(clazz));
     }
 
+    /**
+     * Gets the type adapter for given type
+     *
+     * @param type config type
+     * @return TypeAdapter instance
+     * @param <S> serialized type
+     * @param <R> runtime class
+     *
+     * @since 1.0.0-SNAPSHOT
+     */
     @SuppressWarnings("unchecked")
     public <S, R> TypeAdapter<S, R> getAdapter(ConfigType<R> type) {
         TypeAdapter<?, ?> adapter = this.adapters.get(type);
@@ -115,9 +206,5 @@ public class ConfigurationManager {
         }
 
         return new NativeAdapter<>(type.getType());
-    }
-
-    public Logger getLogger() {
-        return this.logger;
     }
 }
