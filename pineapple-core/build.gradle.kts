95a3adba366f54c39e4d3a00de4803fb5989f8a8
Miles Holder <mwholder2005@gmail.com>
2024-02-02 19:06:55 -0600

    remove checkstyle thing

--- pineapple-core/build.gradle.kts
+++ pineapple-core/build.gradle.kts
@@ -17,14 +17,10 @@ dependencies {
     testImplementation("com.github.seeseemelk:MockBukkit-v1.20:3.9.0")
     compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
 
-    implementation("sh.miles.pineapplechat:pineapplechat-core:1.0.0-SNAPSHOT") { isChanging = true }
-    implementation("sh.miles.pineapplechat:pineapplechat-bungee:1.0.0-SNAPSHOT") { isChanging = true }
+    implementation("sh.miles.pineapplechat:pineapplechat-core:1.0.0-SNAPSHOT")
+    implementation("sh.miles.pineapplechat:pineapplechat-bungee:1.0.0-SNAPSHOT")
     implementation("com.jeff_media:MorePersistentDataTypes:2.4.0")
 
     compileOnly(project(":pineapple-nms:api"))
     compileOnlyAndTest(project(":pineapple-utils"))
-}
-
-configurations.all {
-    resolutionStrategy.cacheChangingModulesFor(0, "seconds")
 }
